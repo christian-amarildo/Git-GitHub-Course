e67fa362c6f6468a3dacaad6ac53cca7823dd581
christian-amarildo <christianamarildo@hotmail.com>
2024-02-27 16:44:28 -0300

    adição de alterações na senha

--- password.py
+++ password.py
@@ -4,13 +4,13 @@ import random
 minusculo = "abcdefghijklmnopqrstuvxyz"
 maiusculo = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
 especiais = "!_-.;:[]()"
-
+numeros = "0123456789"
 # -------------------------------------------------------------
 # AGORA, VOCÊ DEVE ADICIONAR CARACTERES NUMÉRIOS PARA A SENHA.
 # -------------------------------------------------------------
 
 # Junta todos os caracteres em uma única string
-tudo = minusculo + maiusculo + especiais
+tudo = minusculo + maiusculo + especiais +  numeros
 tamanho = 12  # Tamanho da senha
 
 # Inicializa a senha como uma string vazia
